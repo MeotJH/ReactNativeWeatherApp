b9b94fd0c828d74f417ed00d842dd2d1f367708b
KJH <mallangyi@naver.com>
2021-11-27 13:40:38 +0900

    weatherAppFinish

--- App.js
+++ App.js
@@ -1,37 +1,90 @@
-import { StatusBar} from "expo-status-bar"
-import React from 'react';
-import { View, Text, Dimensions,  StyleSheet, ScrollView } from 'react-native';
+import * as Location from 'expo-location';
+import { StatusBar } from "expo-status-bar"
+import React, { useEffect, useState } from 'react';
+import { View, Text, Dimensions, StyleSheet, ScrollView, ActivityIndicator } from 'react-native';
+import { Fontisto } from '@expo/vector-icons'
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 
 export default function App() {
+  const [city, setCity] = useState("Loading...");
+  const [days, setDays] = useState([]);
+  const [ok, setOk] = useState(true);
+  const [date, setDate] = useState([]);
+
+  const API_KEY = "9cada3532028b5953c67de4742964ac0";
+
+  const icons ={
+    "Clear" : "day-sunny",
+    "Clouds" : "cloudy",
+    "Snow" : "snow",
+    "Rain" : "rain",
+  }
+
+  const ask = async() =>{
+    const {granted} = await Location.requestForegroundPermissionsAsync();
+    if(!granted){
+      setOk(false);
+    }
+
+    const {coords:{latitude,longitude}} = await Location.getCurrentPositionAsync({accuracy:5});
+    const location = await Location.reverseGeocodeAsync({latitude,longitude}, {useGoogleMaps:false});
+    setCity(location[0].city);
+
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric`);
+    const json = await response.json();
+    setDays(json.daily);
+    setDate(toStringByFormatting());
+  }
+
+  const toStringByFormatting = () => {
+    
+    let date = new Date();
+    let dateArr = [];
+    const DAY = 1000 * 60 * 60 * 24;
+    let i;
+    for (i = 0; i < 8; i++) {
+        date.setTime(date.getTime() + DAY);
+        dateArr.push(date.toString().substring(0,10));
+    }
+    return dateArr;
+   }
+
+  useEffect(() => {
+    ask();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <View style={styles.city}>
-        <Text style={styles.cityName}>Seoul</Text>
+      <View style={styles.city}>  
+        <Text style={styles.cityName}>{city}</Text>
       </View>
       <ScrollView 
         pagingEnabled
         horizontal
         showsHorizontalScrollIndicator
         contentContainerStyle={styles.weather}>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
+          { days.length === 0 ? (
+          <View style={{...styles.day, alignItems:"center"}}>
+            <ActivityIndicator
+             color="white"
+             style={{ marginTop: 10}}
+             size="large" />
+          </View>
+          ) : (
+          days.map((day, index) =>
+          <View key={index} style={styles.day}>
+            <View style={{  flexDirection: "row", alignItems:"flex-end", width:"80%", justifyContent:"space-between" }}>
+              <Text style={styles.temp}>{parseFloat(day.temp.day).toFixed(1)}</Text>
+              <Fontisto name={icons[day.weather[0].main]} size={80} color="white" />
+            </View>
+            <Text style={styles.description}>{day.weather[0].main}</Text>
+            {/* <Text style={styles.textNomal}>{day.weather[0].description}</Text> */}
+            <Text style={styles.textNomal}>{date[index]}</Text>
+          </View>
+          )
+          )}
       </ScrollView>
       <StatusBar style="light"/>
 
@@ -59,11 +112,11 @@ const styles = StyleSheet.create({
   day:{
     width: SCREEN_WIDTH, 
     flex:1,
-    alignItems: "center",
+    alignItems: "flex-start",
   },
   temp:{
     marginTop: 50,
-    fontSize: 188,
+    fontSize: 150,
     color: "white",
   },
   description:{
@@ -71,4 +124,8 @@ const styles = StyleSheet.create({
     marginTop:-30,
     color: "white",
   },
+  textNomal:{
+    color:"white",
+    fontSize: 20,
+  }
 })
